41b8336cbbd082427f41a1a6175dde8c99269403
Alice Hallam <alicehallam@hotmail.com>
2022-04-30 12:36:03 +0100

    initial commit

---
+++ main.py
@@ -0,0 +1,19 @@
+# coding=utf-8
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print("Hi, {0}".format(name))  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('Alice')
+    for x in range(10):
+        print("{0} is {1}".format(x, "even" if (x % 2) == 0 else "odd"))
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
